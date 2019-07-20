327320ae1e312cf1efc6f3758f2296bacd93267d
Yossi Ben Zaken <yossibz@gmail.com>
2019-07-20 19:35:23 +0300

    Error Handle

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ErrorInterceptorProvide } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -20,7 +21,8 @@ import { RegisterComponent } from './register/register.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      AuthService,
+      ErrorInterceptorProvide
    ],
    bootstrap: [
       AppComponent
